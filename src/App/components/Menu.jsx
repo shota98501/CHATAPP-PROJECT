f86823dcd9531b9ab85f3929f8846b762ba556a4
shota98501 <114859169+shota98501@users.noreply.github.com>
2024-03-20 19:37:18 +0900

    push

--- src/App/components/Menu.jsx
+++ src/App/components/Menu.jsx
@@ -18,21 +18,40 @@ function Menu(props){
     useOnClickOutside(ref, handleClickOutside)
 
     componentDidMount(
-        window.addEventListener('mousedown',props.onClickOutside)
+        window.addEventListener('mousedown', props.onClickOutside)
     )
 
     conponentWillUnmount(
-        window.removeEventListener('mousedown',props.onClickOutside)
+        window.removeEventListener('mousedown', props.onClickOutside)
 
     )
+
+    const {store} = this.props;
+    const user = store.getCurrentUser();
 
     return(
-        <button
-        ref={ref}
-        onClick={handleClickInside}
-        style={{ width: 200, height: 200, background: 'cyan'}}
-        />
+    <div className="user-menu" ref={(ref) => this.ref = ref}>
+        {user ? <div>
+
+            <h2>My menu</h2>
+
+            <ul className="menu">
+                <li><button onClick={() =>{
+                    if(props.onClose){
+                        props.onClose();
+                    }
+
+                    store.signOut();
+                } type="button"> Sign Out</button></li>
+            </ul>
+            
+
+
+        </div> : null}
+        
+    </div>
     )
+
 }
 
 export default Menu;
