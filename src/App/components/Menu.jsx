0fa47dd009765840648f1de3969fb11713b81c83
shota98501 <114859169+shota98501@users.noreply.github.com>
2024-05-05 09:42:56 +0900

    push

--- src/App/components/Menu.jsx
+++ src/App/components/Menu.jsx
@@ -1,3 +1,23 @@
 import React from "react";
 import {useRef} from 'react'
 import { useOnClickOutside } from "usehooks-ts";
+
+function UseMenu(){
+    const ref = useRef(null)
+
+    const handleClickOutside =() => {
+    // your custom logic here
+    console.log('clicked outside')
+}
+
+    const handleClickInside = () =>{
+        // your custom logic here
+        console.log('clicked inside')    
+    }
+
+    useOnClickOutside(ref, handleClickOutside)
+
+    return
+
+}
+
