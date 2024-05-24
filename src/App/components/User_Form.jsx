d61a14365b149b160a3dac75e75cfdc86f4c69c0
shota98501 <114859169+shota98501@users.noreply.github.com>
2024-05-24 10:08:04 +0900

    push

--- src/App/components/User_Form.jsx
+++ src/App/components/User_Form.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
 function UserForm(props){
 
+    const [form, setForm] =useState({
+        message: null,
+        islogin: true,
+        user:{
+            name: '',
+            email:'',
+            password: ''
+        }
+    })
     
 
 }
